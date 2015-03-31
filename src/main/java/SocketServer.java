89c55768972f96ef8cccd3f65a582c614f3ff9ec
MasatoshiTanaka <maasansun3@gmail.com>
2015-03-31 22:14:09 +0900

    no message

--- src/main/java/SocketServer.java
+++ src/main/java/SocketServer.java
@@ -1,12 +1,11 @@
 
+import com.mysql.jdbc.log.Log;
+
 import java.io.*;
 import java.math.BigDecimal;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
@@ -19,89 +18,97 @@ public class SocketServer {
     final static int PORT = 6666;
 
     static String[] lgl22 = new String[]{
-            "select count, placeID, bssid, rssi, channel from lgl22_0825_50_1",
-            "select count, placeID, bssid, rssi, channel from lgl22_0825_50_2",
+            "select count, placeID, bssid, rssi, freq from lgl22_1",
+            "select count, placeID, bssid, rssi, freq from lgl22_2"
     };
 
     static String[] nexus2012 = new String[]{
-            "select count, placeID, bssid, rssi, channel from nexus2012_0825_50_1",
-            "select count, placeID, bssid, rssi, channel from nexus2012_0825_50_2",
+            "select count, placeID, bssid, rssi, freq from nexus2012_1",
+            "select count, placeID, bssid, rssi, freq from nexus2012_2"
     };
 
     static String[] nexus2013 = new String[]{
-            "select count, placeID, bssid, rssi, channel from nexus2013_0825_50_1",
-            "select count, placeID, bssid, rssi, channel from nexus2013_0825_50_2"
+            "select count, placeID, bssid, rssi, freq from nexus2013_1",
+            "select count, placeID, bssid, rssi, freq from nexus2013_2"
     };
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+        Logger logger = Logger.getLogger("");
         try {
-            System.out.println("クライアントからの接続をポート6666で待ちます");
             ServerSocket serverSocket = new ServerSocket(PORT);
+            Map<String, Map<String, Map<Integer, BigDecimal>>> fingerPrint = createFingerPrint();
             while (true) {
                 Socket socket = serverSocket.accept();
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                Map<String, List<Integer>> estimateWiFiData = (Map<String, List<Integer>>) (objectInputStream.readObject());
+
+                List<Map.Entry> result = comparison(fingerPrint, estimateWiFiData);
+
                 try {
-                    //ArrayList<String> arrayList = new ArrayList<String>();
-
-                    // 受信
-                    ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-                    Map<String, List<Integer>> estimateWiFiData = (Map<String, List<Integer>>) (objectInputStream.readObject());
-
-                    for (String bssid : estimateWiFiData.keySet()) {
-                        List<Integer> rssiList = estimateWiFiData.get(bssid);
-                        System.out.println(bssid + " " + rssiList);
-                    }
-
-                    Map<String, Map<String, List<Integer>>> fingerPrintWiFiData = new TreeMap();
-                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/wifidata", "root", "root");
-                    PreparedStatement preparedStatement = connection.prepareStatement(nexus2012[1]);
-                    ResultSet resultSet = preparedStatement.executeQuery();
-                    while (resultSet.next()) {
-                        String placeID = resultSet.getString("placeID");
-                        int count = resultSet.getInt("count");
-                        String bssid = resultSet.getString("bssid");
-                        int rssi = resultSet.getInt("rssi");
-                        int channel = resultSet.getInt("channel");
-                        if (!fingerPrintWiFiData.containsKey(placeID)) {
-                            Map<String, List<Integer>> map = new TreeMap();
-                            List<Integer> list = new ArrayList(Arrays.asList(rssi));
-                            map.put(bssid, list);
-                            fingerPrintWiFiData.put(placeID, map);
-                        } else {
-                            if (!fingerPrintWiFiData.get(placeID).containsKey(bssid)) {
-                                List<Integer> list = new ArrayList(Arrays.asList(rssi));
-                                fingerPrintWiFiData.get(placeID).put(bssid, list);
-                            } else {
-                                fingerPrintWiFiData.get(placeID).get(bssid).add(rssi);
-                            }
-                        }
-                    }
-
-                    FundamentalStatics fundamentalStatics = new FundamentalStatics(fingerPrintWiFiData);
-                    List<FundamentalStatics> fundamentalStaticsList = fundamentalStatics.getFundamentalStaticsList();
-                    FingerPrint fingerPrint = new FingerPrint();
-                    Map<String, Map<String, Map<Integer, BigDecimal>>> fingerPrintData = fingerPrint.getNormallyDistributionFingerPrint(fundamentalStaticsList);
-                    List<Map.Entry> result = comparison(fingerPrintData, estimateWiFiData);
-                    try {
-                        System.out.println(result.get(0).getKey());
-                        OutputStream outputStream = socket.getOutputStream();
-                        outputStream.write(((String) result.get(0).getKey()).getBytes());
-                        System.out.println(((String) result.get(0).getKey()));
-                        outputStream.flush();
-                    }catch(Exception e) {
-                        OutputStream outputStream = socket.getOutputStream();
-                        outputStream.write("推定できません".getBytes());
-                        System.out.println("推定できません");
-                        outputStream.flush();
-                    }
-                    // ソケット削除
-                    socket.close();
+                    System.out.println(result.get(0).getKey());
+                    OutputStream outputStream = socket.getOutputStream();
+                    outputStream.write(((String) result.get(0).getKey()).getBytes());
+                    outputStream.flush();
+                    logger.info(((String) result.get(0).getKey()));
                 } catch (Exception e) {
-
+                    OutputStream outputStream = socket.getOutputStream();
+                    outputStream.write("推定できませんでした".getBytes());
+                    outputStream.flush();
+                    logger.info("推定できませんでした");
                 }
+                socket.close();
             }
-        } catch (Exception e) {
+        } catch (ClassNotFoundException e) {
+            logger.info("ClassNotFoundException");
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.info("IOException");
             e.printStackTrace();
         }
+    }
+
+    public static Map<String, Map<String, Map<Integer, BigDecimal>>> createFingerPrint(){
+        Logger logger = Logger.getLogger(" createFingerPrint");
+        logger.info("基準データ作成開始");
+        Map<String, Map<String, Map<Integer, BigDecimal>>> fingerPrintData = null;
+        try {
+            Map<String, Map<String, List<Integer>>> fingerPrintWiFiData = new TreeMap();
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/wifidata", "root", "root");
+            PreparedStatement preparedStatement = connection.prepareStatement(lgl22[0]);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String placeID = resultSet.getString("placeID");
+                int count = resultSet.getInt("count");
+                String bssid = resultSet.getString("bssid");
+                int rssi = resultSet.getInt("rssi");
+                int freq = resultSet.getInt("freq");
+                if (!fingerPrintWiFiData.containsKey(placeID)) {
+                    Map<String, List<Integer>> map = new TreeMap();
+                    List<Integer> list = new ArrayList(Arrays.asList(rssi));
+                    map.put(bssid, list);
+                    fingerPrintWiFiData.put(placeID, map);
+                } else {
+                    if (!fingerPrintWiFiData.get(placeID).containsKey(bssid)) {
+                        List<Integer> list = new ArrayList(Arrays.asList(rssi));
+                        fingerPrintWiFiData.get(placeID).put(bssid, list);
+                    } else {
+                        fingerPrintWiFiData.get(placeID).get(bssid).add(rssi);
+                    }
+                }
+            }
+
+
+            FundamentalStatics fundamentalStatics = new FundamentalStatics(fingerPrintWiFiData);
+            List<FundamentalStatics> fundamentalStaticsList = fundamentalStatics.getFundamentalStaticsList();
+            FingerPrint fingerPrint = new FingerPrint();
+            fingerPrintData = fingerPrint.getNormallyDistributionFingerPrint(fundamentalStaticsList);
+            logger.info("基準データ作成成功");
+        } catch (SQLException e) {
+            logger.warning("基準データ作成に失敗しました");
+            System.exit(0);
+        }
+        return fingerPrintData;
     }
 
     public static List<Map.Entry> comparison(Map<String, Map<String, Map<Integer, BigDecimal>>> fingerPrintData, Map<String, List<Integer>> estimateWiFiData){
